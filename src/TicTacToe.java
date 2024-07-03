28ebde9f61a838af77d5359b2e35e52564621e44
Jeffrey <jeffreychen846@gmail.com>
2024-07-02 23:48:33 -0400

    Initial commit

---
+++ src/TicTacToe.java
@@ -0,0 +1,11 @@
+public class TicTacToe
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    private static final int ROW = 3;
+    private  static final int COL = 3;
+    private static String board[][] = new String[ROW][COL];
+}
